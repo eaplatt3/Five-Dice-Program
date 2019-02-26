0672eb3b1398354c69029281831cbb4482b9b3b4
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-02-25 19:33:47 -0500

    Added Dice Total

--- FiveDiceProgram.pl
+++ FiveDiceProgram.pl
@@ -8,6 +8,7 @@ my $die3;
 my $die4;
 my $die5;
 my $num = 5;
+my $dieTot;
 
 # Assigning Random Number
 $die1 = int rand($num) + 1;
@@ -16,15 +17,20 @@ $die3 = int rand($num) + 1;
 $die4 = int rand($num) + 1;
 $die5 = int rand($num) + 1;
 
+#Adding Values
+$dieTot = $die1 + $die2 + $die3 + $die4 + $die5;
+
 # Print Die & Number
 print "\n";
-print "Die 1 " . "is: " . $die1;
+print "Die 1 is: " . $die1;
 print "\n";
-print "Die 2 " . "is: " . $die2;
+print "Die 2 is: " . $die2;
 print "\n";
-print "Die 3 " . "is: " . $die3;
+print "Die 3 is: " . $die3;
 print "\n";
-print "Die 4 " . "is: " . $die4;
+print "Die 4 is: " . $die4;
 print "\n";
-print "Die 5 " . "is: " . $die5;
+print "Die 5 is: " . $die5;
+print "\n";
+print "The Totals of the 5 Dice is: " . $dieTot;
 print "\n";
