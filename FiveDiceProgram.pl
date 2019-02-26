957a2ddcda3b9700d94725138ba2cb57c4abf5c4
Earl Platt III <36335481+eaplatt3@users.noreply.github.com>
2019-02-25 19:29:22 -0500

    Adjusted Die Numbers

---
+++ FiveDiceProgram.pl
@@ -0,0 +1,30 @@
+use strict;
+use warnings; 
+
+# Variables 
+my $die1;
+my $die2;
+my $die3;
+my $die4;
+my $die5;
+my $num = 5;
+
+# Assigning Random Number
+$die1 = int rand($num) + 1;
+$die2 = int rand($num) + 1;
+$die3 = int rand($num) + 1;
+$die4 = int rand($num) + 1;
+$die5 = int rand($num) + 1;
+
+# Print Die & Number
+print "\n";
+print "Die 1 " . "is: " . $die1;
+print "\n";
+print "Die 2 " . "is: " . $die2;
+print "\n";
+print "Die 3 " . "is: " . $die3;
+print "\n";
+print "Die 4 " . "is: " . $die4;
+print "\n";
+print "Die 5 " . "is: " . $die5;
+print "\n";
